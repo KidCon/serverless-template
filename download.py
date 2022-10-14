8ce5adf7f4e1dbbd61c86f09276afb9613d93608
Rees <rees.pawson@blackbook.ai>
2022-10-14 18:37:54 +0800

    removed huggingFace stuff in download.py

--- download.py
+++ download.py
@@ -7,7 +7,8 @@ from transformers import pipeline
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
+    # pipeline('fill-mask', model='bert-base-uncased')
 
 if __name__ == "__main__":
-    download_model()
+    # download_model()
+    print("Hello world from __main__ in download.py")
